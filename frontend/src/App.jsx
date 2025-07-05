9e5e5cf36ed8b406d649c9b7adcee382ae2335c2
Ankit <ankitrajdihara123@gmail.com>
2025-07-05 15:54:05 +0530

    svg updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 // import Navbar from "./components/Navbar";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import AllUrls from "./pages/Allurl";
